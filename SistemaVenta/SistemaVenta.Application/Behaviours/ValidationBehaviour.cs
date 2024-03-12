03e8f169725d7bd66dd858bb793e447c2b68b776
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2024-03-12 13:39:35 -0500

    Add: Adding Exceptions

--- SistemaVenta/SistemaVenta.Application/Behaviours/ValidationBehaviour.cs
+++ SistemaVenta/SistemaVenta.Application/Behaviours/ValidationBehaviour.cs
@@ -1,11 +1,5 @@
 ﻿using FluentValidation;
 using MediatR;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SistemaVenta.Application.Behaviours
 {
---
+++ SistemaVenta/SistemaVenta.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation.Results;
+
+namespace SistemaVenta.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException() : base("One or more validation errors occurred")
+        {
+            Errors = new List<KeyValuePair<string, string>>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+        {
+            Errors = failures.Select(f => new KeyValuePair<string, string>(f.PropertyName, f.ErrorMessage));
+        }
+
+        public ValidationException(IEnumerable<KeyValuePair<string, string>> errorList) : this()
+        {
+            Errors = errorList;
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> Errors { get; }
+
+    }
+}

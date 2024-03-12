c713b0c3f5942c41c5871f5c7bcf3312a9782ccd
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2024-03-12 14:25:06 -0500

    Add: Add Dependency Injection DbContext

---
+++ SistemaVenta/SistemaVenta.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVenta.Infrastructure.Persistence;
+
+namespace SistemaVenta.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<SistemaVentaDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ConnectionString"))
+            );
+            return services;
+        }
+
+    }
+}
---
+++ SistemaVenta/SistemaVenta.Infrastructure/Persistence/SistemaVentaDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaVenta.Domain;
+
+namespace SistemaVenta.Infrastructure.Persistence
+{
+    public class SistemaVentaDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleDetail> SaleDetails { get; set; }
+        public SistemaVentaDbContext(DbContextOptions<SistemaVentaDbContext> options) : base(options) { }   
+    }
+
+
+}
